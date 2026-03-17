cc3617937fd0cedc781ded64b5e16d30af2b163f
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-03-17 13:49:21 -0500

    check greater num from inputs

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-temperature = 35
-
-if temperature > 30:
-    response = "It's really hot today."
-else:
-    response = "It's not so hot today."
-
-print(response)
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+if firstNumber > secondNumber:
+    print(firstNumber)
+    print(secondNumber)
+elif secondNumber > firstNumber:
+    print(secondNumber)
+    print(firstNumber)
+else:                        # If both numbers are equal, we can print either one of them
+    print(firstNumber)
+    print(secondNumber)
