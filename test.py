d8393662efa22abe97fc4b66ee59d29a47c9d9a4
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-03-17 14:55:11 -0500

    converting signs of numbers

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-year = int(input())
-
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap")
-else:
-    print("Ordinary")
+number = int(input())
+if number >= 0:
+    number = -1 * number
+    print(number)
+    print("The number is negative")
+elif number < 0:
+    number = -1 * number
+    print(number)
+    print("The number is positive")
+    
