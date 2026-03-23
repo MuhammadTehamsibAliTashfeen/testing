838a47ede0b91eac5797337861db8a8b9f554e33
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-03-23 11:06:29 -0500

    number checker

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-dog_breeds = ["Labrador Retriever", "German Shepherd", "Golden Retriever", "Bulldog", "Beagle"]
-print(dog_breeds)
+# Function to test if a number is Positive, Negative, or Zero
+def check_num(n):
+    if n > 0:
+        print("Positive")
+    elif n < 0:
+        print("Negative")
+    else:
+        print("Zero")
+
+# Take input from user
+num = int(input())
+
+# Call the function
+check_num(num)
