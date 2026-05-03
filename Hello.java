7a6f7ac33f675b0c9bb7dd31195aec85b7a16f60
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-05-02 21:24:01 -0500

    Java Hello World Message

--- Hello.java
+++ Hello.java
@@ -1,25 +1,5 @@
-class Test
-{
-    public static void main(String[] args)
-    {
-        int num1 = 1;
-        float num2 = 2.5f;
-        char ch = 'A';
-        byte num3 = 100;
-        double num4 = 3.14;
-        long num5 = 100000l;
-        boolean flag = true;
-
-
-        System.out.println("Integer: " + num1);
-        System.out.println("Float: " + num2);
-        System.out.println("Character: " + ch);
-        System.out.println("Byte: " + num3);
-        System.out.println("Double: " + num4);
-        System.out.println("Long: " + num5);
-
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
-
-
-
 }
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-# Function to test if a number is Positive, Negative, or Zero
-def check_num(n):
-    if n > 0:
-        print("Positive")
-    elif n < 0:
-        print("Negative")
-    else:
-        print("Zero")
-
-# Take input from user
-num = int(input())
-
-# Call the function
-check_num(num)
+# put your python code here
+a = int(input())
+b = int(input())
+c = int(input())    
+sum = a + b + c
+print(sum)
