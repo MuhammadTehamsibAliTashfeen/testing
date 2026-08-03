b9c5533958b20e993acc1ada2cfe7f09d2b9c97a
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-02 20:35:31 -0500

    Lets start playing with literals in java dataTypes

--- Hello.java
+++ Hello.java
@@ -1,7 +1,28 @@
 class hello
 {
-   public static void main(String[] args)
+   public static void main(String args[])
     {
-        System.out.println("Hello Ali");
+        // lets work with literals
+        byte b = 3;
+        short s =342;
+        int i = 1232131;
+        long l = 123123123123L;
+
+        char c = 'a';
+
+        float f = 3.123f;
+        double d = 12312312.34234234;
+
+        boolean bo = true;
+
+
+        
+
+
+
+
+
+
+
     }
 }
