5b540573711f3d4bdb9bd8ac72421244645969dc
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-08 18:03:24 -0500

    boolean testing

--- Hello.java
+++ Hello.java
@@ -2,27 +2,10 @@ public class Hello {
 
     public static void main(String[] args){
 
-        //Below code prints my name and age
-        //System.out.println("My name is: Muhammad Tehmasib Ali Tashfeen");
-        //System.out.println("My age is: " + 28);
 
-        /*The Below code
-        will get more complicated */
+        boolean Is_race_brown = true ;
 
-        //type variableName = value;
-        String name = "Muhammad Tehmasib";
-        System.out.println(name);
-
-        long myNumber = 3162148670L;
-        System.out.println(myNumber);
-
-        int age;
-        age = 28;
-        System.out.println(age);
-
-        //to make a value unchangeable/constant/final
-        final int SSN = 721312;
-        System.out.println(SSN);
+        System.out.println(Is_race_brown);
 
     }
 }
