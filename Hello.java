acef8b8aa4704eba71d9b89f15aca26e7e4998e0
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-07 22:48:06 -0500

    testing unchangeable variables

--- Hello.java
+++ Hello.java
@@ -3,13 +3,26 @@ public class Hello {
     public static void main(String[] args){
 
         //Below code prints my name and age
-        System.out.println("My name is: Muhammad Tehmasib Ali Tashfeen");
-        System.out.println("My age is: " + 28);
+        //System.out.println("My name is: Muhammad Tehmasib Ali Tashfeen");
+        //System.out.println("My age is: " + 28);
 
         /*The Below code
         will get more complicated */
 
+        //type variableName = value;
+        String name = "Muhammad Tehmasib";
+        System.out.println(name);
 
+        long myNumber = 3162148670L;
+        System.out.println(myNumber);
+
+        int age;
+        age = 28;
+        System.out.println(age);
+
+        //to make a value unchangeable/constant/final
+        final int SSN = 721312;
+        System.out.println(SSN);
 
     }
 }
