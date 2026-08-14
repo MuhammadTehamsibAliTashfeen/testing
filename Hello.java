c09a80a439e617119cde2fe4825eb93d95af2aee
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-14 12:00:00 -0500

    playing with methods

--- Hello.java
+++ Hello.java
@@ -1,23 +1,18 @@
 
 
- class Caluculator{
-
-     public int add(int n1, int n2) {
-        int sum = n1 + n2;
-        return sum;
+public class Hello
+{
+    static void myMethod()
+    {
+        System.out.println("Hello");
     }
-}
 
-public class Hello {
+    public static void main(String[] args)
+    {
 
-    public static void main(String[] args) {
+        myMethod();
+        myMethod();
 
-
-        Caluculator cal = new Caluculator();
-
-        int result = cal.add(3,5);
-
-        System.out.println(result);
 
     }
 }
