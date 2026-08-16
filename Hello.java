84cbd3aef5f7352c591605ee21d82b7612688f81
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-16 15:40:30 -0500

    arrays ops

--- Hello.java
+++ Hello.java
@@ -3,23 +3,16 @@ public class Hello
 
      public static void main(String[] args){
 
-          String[] cars = {"BMW", "Volvo","AMG","Ford"};
-
-          for (int i = 0; i < cars.length; i++)
-          {
-               System.out.println(cars[i]);
+          int[] age = {22,34,45,12,12,34};
+          int sum = 0;
+          int average;
+          for (int i = 0; i < age.length; i++){
+                sum += age[i];
           }
+          average = sum/2;
 
-          String[] trucks = new String[4];
-          for (int i = 0; i < trucks.length; i++)
-          {
-               System.out.println(trucks[i]);
-          }
-          trucks[0] = "Man";
-          trucks[3] = "Catapiller";
-          System.out.println(trucks[2]);
-          System.out.println(trucks[3]);
-
+          System.out.println(sum);
+          System.out.println(average);
 
      }
 }
