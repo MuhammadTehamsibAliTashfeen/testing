694118c2a83519a9fcfff2dad6fc826c63a42cff
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-22 19:51:23 -0500

    array revision

--- Hello.java
+++ Hello.java
@@ -3,13 +3,9 @@ public class Hello
 
      public static void main(String[] args){
 
-          int[][] homes = {{1,2,10},{4,5,6}};
+          int[] num = new int[14];
 
-          for ( int row = 0; row < homes.length; row++){
-               for (int col = 0; col < homes[row].length; col++){
-                    System.out.println(homes[row][col]);
-               }
-          }
+
 
 
 
