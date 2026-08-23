6e6b293f26e56efe21156fd24101ed7b6a06ac5e
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-23 01:35:05 -0500

    challenge2

--- Hello.java
+++ Hello.java
@@ -1,14 +1,39 @@
 public class Hello{
 
     public static void main(String[] args){
-        sayHello("Muhammad",14);
-        sayHello("Ali",33);
-        sayHello("Aimen",21);
-
+    greet("Muhammad");
+    int result = square(5);
+    System.out.println(result);
+    boolean result1 = isEven(2);
+    System.out.println(result1);
+    int result2 = findMaximium(4,6);
+    System.out.println(result2);
+    double result3 = calculateAverage(10,55,97);
+    System.out.println(result3);
     }
 
-    public static void sayHello(String name, int age){
-        System.out.println("Hello! " + name);
-        System.out.println("I am "+age+" years old.");
+    public static void greet(String name){
+        System.out.println("Hello, "+name+"!");
+    }
+
+    public static int square(int a){
+        return a * a;
+    }
+
+    public static boolean isEven(int num){
+        return num % 2 == 0;
+    }
+
+    public static int findMaximium(int a, int b){
+        if (a>b){
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+
+    public static double calculateAverage(int a, int b, int c){
+        return (a+b+c)/3.0;
     }
 }
