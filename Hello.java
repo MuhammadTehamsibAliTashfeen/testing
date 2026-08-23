1f1a556d17aafef62ede0ca1cf811d74f7c767e9
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-22 23:55:59 -0500

    method for greaating and age

--- Hello.java
+++ Hello.java
@@ -1,13 +1,14 @@
 public class Hello{
 
     public static void main(String[] args){
-        sayHello("Muhammad");
-        sayHello("Ali");
-        sayHello("Aimen");
+        sayHello("Muhammad",14);
+        sayHello("Ali",33);
+        sayHello("Aimen",21);
 
     }
 
-    public static void sayHello(String name){
-        System.out.println("Hello!" + name);
+    public static void sayHello(String name, int age){
+        System.out.println("Hello! " + name);
+        System.out.println("I am "+age+" years old.");
     }
 }
