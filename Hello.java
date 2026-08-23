a65e255777b18320135bf2e1d0bdfe4b9ab11cdd
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-22 21:32:58 -0500

    array revision

--- Hello.java
+++ Hello.java
@@ -3,11 +3,18 @@ public class Hello
 
      public static void main(String[] args){
 
-          int[] num = new int[14];
+          int num[][] = new int[3][4];
+          int temp;
 
+          for (int r = 0; r < 3; r++){
 
+               for (int c = 0; c < 4; c++){
+                    num[r][c] = (int) (Math.random() * 100);
+                    System.out.print(num[r][c] + " ");
+               }
+               System.out.println();
 
-
+          }
 
 
 
--- out/test/test/test.py
+++
@@ -1,6 +0,0 @@
-# put your python code here
-a = int(input())
-b = int(input())
-c = int(input())    
-sum = a + b + c
-print(sum)
