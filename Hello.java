684d976ad440f04eddc9b96d6ec95b610b1a385c
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-23 20:29:19 -0500

    reverse the numbers challange

--- Hello.java
+++ Hello.java
@@ -1,63 +1,17 @@
-public class Hello{
+public class Hello {
 
-    public static void main(String[] args){
-
-        boolean result = isPositive(-4);
-        System.out.println(result);
-
-        boolean rr2 = isPrime(9);
-        System.out.println(rr2);
-
-
-        int result3 = factorial(5);
-        System.out.println(result3);
-
-        int result4 = findMinimum(3,1);
-        System.out.println(result4);
-
-        double result5 = rectangleArea(33,11);
-        System.out.println(result5);
+    public static void main(String[] args) {
+        System.out.println(reverseNumber(12345));
 
     }
 
-    public static boolean isPositive(int num){
-         return num > 0;
-         }
 
-
-   public static boolean isPrime(int num) {
-         if (num <= 1) {
-             return false;
-         }
-         for (int n = 2; n < num; n++){
-             if ( num % n == 0){
-                 return false;
-             }
-         }
-
-         return true;
-     }
-
-     public static int factorial(int num){
-        int r = 1;
-             for (int n = 1; n <=num; n++){
-                  r *= n;
-             }
-             return r;
+    public static int reverseNumber(int n) {
+        int reverse = 0;
+        while (n > 0) {
+            reverse = n % 10 + reverse * 10;
+            n = n / 10;
         }
-
-         public static int findMinimum(int a, int b){
-             if (a < b){
-                 return a;
-             }else {
-                 return b;
-             }
-        }
-         public static double rectangleArea(double a, double b){
-             double area = a * b;
-             return area;
-        }
-
-
-
+        return  reverse;
     }
+}
