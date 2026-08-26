e1f51372e1e01b0f544439a4c576dd192ab26a7e
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-25 23:41:37 -0500

    testing

--- Hello.java
+++ Hello.java
@@ -1,8 +1,28 @@
+/*
+Arrays
+├── Creating arrays
+├── Indexing
+├── Reading elements
+├── Modifying elements
+├── Array length
+├── for loops + arrays
+├── for-each loops
+├── Passing arrays to methods
+├── Searching arrays
+├── Finding min/max
+├── Summing arrays
+├── Reversing arrays
+├── Copying arrays
+└── Multidimensional arrays
+ */
+
+
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println(reverseNumber(12345));
-        System.out.println(isPalindrome(12345));
+        ystem.out.println(isPalindrome(12345));
 
     }
 
