b09119ba734aa78dcc8b9440fa369ede4846ffa3
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-08-29 19:47:32 -0500

    init arrays

--- Hello.java
+++ Hello.java
@@ -21,28 +21,14 @@ Arrays
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println(reverseNumber(12345));
-        ystem.out.println(isPalindrome(12345));
+
+        int[] score = {80, 22,33,100};
+        System.out.println(score);
+
+        double[] money = new double[7];
+
 
     }
 
 
-    public static int reverseNumber(int n) {
-        int reverse = 0;
-        while (n > 0) {
-            reverse = n % 10 + reverse * 10;
-            n = n / 10;
-        }
-        return  reverse;
     }
-
-    public static boolean isPalindrome(int n){
-        int original = n;
-        int result = reverseNumber(n);
-        if(original == result){
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
