9d3400e6c83daaa8ad77ad2bcb2c8139855ee146
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-09-21 22:23:54 -0500

    generating random numbers for 2D arrays

--- Hello.java
+++ Hello.java
@@ -1,11 +1,19 @@
 public class Hello {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int num[] = {1,2,3};
+        int num[][] = new int[3][4];
 
-        int prices[] = new int[4];
+        for(int i = 0 ; i < num.length; i++){
+            for (int j = 0 ; j < num[i].length; j++){
+                num[i][j] = (int) (Math.random() * 100);
+                System.out.print(num[i][j] + " ");
+
+            }
+            System.out.println();
+    }
+
 
 
 
