5a6ab703ea4b69e4028a7c20c3dd5149d45658e7
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-09-21 22:40:38 -0500

    for enhanced loop 4 2D array

--- Hello.java
+++ Hello.java
@@ -8,11 +8,15 @@ public class Hello {
         for(int i = 0 ; i < num.length; i++){
             for (int j = 0 ; j < num[i].length; j++){
                 num[i][j] = (int) (Math.random() * 100);
-                System.out.print(num[i][j] + " ");
 
             }
-            System.out.println();
     }
+        for( int n[]: num){
+            for ( int m: n){
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
 
 
 
