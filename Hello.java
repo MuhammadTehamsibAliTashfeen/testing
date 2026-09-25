9a1ffe971caa817e255869205e9d414aa168c1b8
Muhammad Tehmasib Ali Tashfeen <alitashfeenm@gmail.com>
2026-09-24 22:47:56 -0500

    Revision

--- Hello.java
+++ Hello.java
@@ -1,34 +1,15 @@
+//Writen and maintained by Ali
+
 public class Hello {
 
-
     public static void main(String[] args) {
+        
+        String name = "Ali Tashfeen";
 
-        int num[][][] = new int[3][2][5];
+        System.out.println(name);
 
-
-
-
-
-        for(int i = 0 ; i < num.length; i++){
-            for (int j = 0 ; j < num[i].length; j++){
-                for (int k = 0; k < num[i][j].length; k++)
-                num[i][j][k] = (int) (Math.random() * 100);
-
-            }
-    }
-
-    for(int[][] n : num) {
-        for (int[] m : n) {
-            for (int y : m) {
-                System.out.print(y + " ");
-            }
-        }
-        System.out.println();
-    }
-
-
-
-
+    
+    
 
 
     }
